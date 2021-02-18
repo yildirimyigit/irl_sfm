233ea41af431a075026eb91bb2c5f7b771922a9a
yigit <yigyil@gmail.com>
2021-02-18 10:32:42 +0300

    test conductor is changed for headless simulation tests

--- catkin_ws/src/sfm_planner/src/recorder.py
+++ catkin_ws/src/sfm_planner/src/recorder.py
@@ -8,42 +8,25 @@ import time
 
 
 GOAL = np.array([rospy.get_param('/goal/position/x', 0), rospy.get_param('/goal/position/y', 13.0)])
-# OBS = np.array([rospy.get_param('/obstacle/position/x', 1), rospy.get_param('/obstacle/position/y', 0.0)]) # obstacle pose
-SOURCE = np.array([rospy.get_param('/start/position/x', 0), rospy.get_param('/start/position/y', 0.0)])
+OBS = np.array([rospy.get_param('/obstacle/position/x', 0.0), rospy.get_param('/obstacle/position/y', 0.0)]) # obstacle pose
+SOURCE = np.array([rospy.get_param('/start/position/x', 0.0), rospy.get_param('/start/position/y', 0.0)])
 
 
 class Recorder:
-    '''
-        @param obs_pose = np.array([x, y])
-    '''
-    def __init__(self, in_data_path='/home/yigit/Documents/projects/irl_sfm/data/demonstrations/sfm/'):
+    def __init__(self):
         self.demonstration = []  # [[d_g_x, d_g_y, d_o_x, d_o_y, vx, vy, x, y], ...]
         self.last_vel_x, self.last_vel_y = 0, 0
 
         self.is_saved = False
-        self.obstacle_pose_read = False
-        self.obstacle_pose = PoseStamped()
-        self.OBS = np.zeros(2)
+        
+        self.data_path_root = rospy.get_param('/recorder_root_path')
 
         self.pose_subscriber = rospy.Subscriber("/robotPose", PoseStamped, self.pose_callback)
         self.vel_subscriber = rospy.Subscriber("/cmd_vel", Twist, self.vel_callback)
-        self.obs_0_pose_subs = rospy.Subscriber("/obstacle_0_pose", PoseStamped, self.obs_0_pose_callback)
         self.goal_reached_subscriber = rospy.Subscriber("/sfm/goal_reached", Bool, self.goal_reached_callback)
-
-        self.data_path_root = rospy.get_param('/recorder_root_path', in_data_path)
 
     def vel_callback(self, msg):
         self.last_vel_x, self.last_vel_y = msg.linear.x, msg.linear.y
-
-    def obs_0_pose_callback(self, msg):
-        if not self.obstacle_pose_read:  # run once
-            self.obstacle_pose = msg
-            data_path_prefix = 'sx_' + str(SOURCE[0]) + '_sy_' + str(SOURCE[1]) + '_gx_' + str(GOAL[0]) + '_gy_' + str(GOAL[1])
-            self.OBS = np.array([self.obstacle_pose.pose.position.x, self.obstacle_pose.pose.position.y])
-            data_path_suffix = 'x_' + str(round(self.OBS[0], 1)) + '_y_' + str(round(self.OBS[1], 1))
-            self.data_path = self.data_path_root + data_path_prefix + '_' + data_path_suffix
-
-            self.obstacle_pose_read = True
 
     def goal_reached_callback(self, msg):
         if msg.data:  # is True
@@ -52,24 +35,26 @@ class Recorder:
             rospy.signal_shutdown('Goal Reached!')
 
     def pose_callback(self, msg):
-        if self.obstacle_pose_read:
-            if msg.pose.position.y >= SOURCE[1]:
-                pose_x = msg.pose.position.x
-                pose_y = msg.pose.position.y
-                pose_arr = np.array([pose_x, pose_y])
-                d_g_x, d_g_y = GOAL - pose_arr
-                d_o_x, d_o_y = self.OBS - pose_arr
+        if msg.pose.position.y >= SOURCE[1]:
+            pose_x = msg.pose.position.x
+            pose_y = msg.pose.position.y
+            pose_arr = np.array([pose_x, pose_y])
+            d_g_x, d_g_y = GOAL - pose_arr
+            d_o_x, d_o_y = OBS - pose_arr
 
-                vx, vy = self.last_vel_x, self.last_vel_y
+            vx, vy = self.last_vel_x, self.last_vel_y
 
-                state = np.array([d_g_x, d_g_y, d_o_x, d_o_y, vx, vy, pose_x, pose_y])
-                self.demonstration.append(state)
+            state = np.array([d_g_x, d_g_y, d_o_x, d_o_y, vx, vy, pose_x, pose_y])
+            self.demonstration.append(state)
 
     def save(self):
-        np.save(self.data_path, np.array(self.demonstration))
+        filename = str(SOURCE[0]) + '_' + str(GOAL[0]) + '_' + str(OBS[0]) + '_' + str(OBS[1]) + '.npy'
+        save_path = self.data_path_root + rospy.get_param('/scenario') + '/' + filename
+
+        np.save(save_path, np.array(self.demonstration))
 
 
 if __name__ == '__main__':
     rospy.init_node('sfm_data_recorder', anonymous=True)
-    r = Recorder('/home/yigit/Documents/projects/irl_sfm/data/demonstrations/sfm/small_env_changing_s_g/')
+    r = Recorder()
     rospy.spin()
--- catkin_ws/src/sfm_planner/src/test_conductor.py
+++ catkin_ws/src/sfm_planner/src/test_conductor.py
@@ -4,6 +4,7 @@ from sfm import SFMController
 from recorder import Recorder
 
 import os
+import os.path
 import errno
 import sys
 import time
@@ -19,34 +20,59 @@ except:
 
 
 class Conductor:
-    def __init__(self, root_path, cid):
-        self.root_path = root_path
-        rospy.set_param('/recorder_root_path', self.root_path)
+    def __init__(self, cid):
+#        self.scenario = 'small_env_changing_s_g/headless'
+        self.scenario = 'small_env_changing_s_g'
+        
+        rospy.set_param('/scenario', self.scenario)
+
+        self.num_max_tests = int(1e6)
+
+        self.source = [-3, 3]
+        self.goal = [-3, 3]
+        self.obs_x = [-3, 3]
+        self.obs_y = [2, 10]
+
+        self.client_id = cid
+        self.root_path = rospy.get_param('/recorder_root_path')
 
         try:
             os.mkdir(self.root_path)
-            os.mkdir(self.root_path + 'novel/')
+            os.mkdir(self.root_path + self.scenario + '/')
         except OSError as exc:
             if exc.errno == errno.EEXIST:
-                print('Folder exists. Moving on with \n', root_path)
+                print('Folder exists. Moving on with \n', self.root_path)
                 pass
             else:
                 raise exc
                 sys.exit(1)
 
-        x = np.hstack((np.arange(-3.0, 0.6, 0.1), np.arange(1.5, 3.1, 0.1)))
-        nx = np.array([1])
-        y = np.arange(2, 10.1, 0.5)
-        ny = np.array([6])
-        self.obs_poses = np.array([[x0, y0] for x0 in x for y0 in y])
+    def get_test_config(self):
+        source_x = np.round(np.random.uniform(self.source[0], self.source[1]), 2)
+        goal_x = np.round(np.random.uniform(self.goal[0], self.goal[1]), 2)
+        obs_x = np.round(np.random.uniform(self.obs_x[0], self.obs_x[1]), 2)
+        obs_y = np.round(np.random.uniform(self.obs_y[0], self.obs_y[1]), 2)
+        
+        filename = str(source_x) + '_' + str(goal_x) + '_' + str(obs_x) + '_' + str(obs_y) + '.npy'
+        save_path = self.root_path + self.scenario + '/' + filename
+        
+        while os.path.isfile(save_path):  # if file exists, that config is used earlier
+            source_x = np.round(np.random.uniform(self.source[0], self.source[1]), 2)
+            goal_x = np.round(np.random.uniform(self.goal[0], self.goal[1]), 2)
+            obs_x = np.round(np.random.uniform(self.obs_x[0], self.obs_x[1]), 2)
+            obs_y = np.round(np.random.uniform(self.obs_y[0], self.obs_y[1]), 2)
+            
+            filename = str(source_x) + '_' + str(goal_x) + '_' + str(obs_x) + '_' + str(obs_y) + '.npy'
+            save_path = self.root_path + self.scenario + '/' + filename
 
-        self.novel_obs_pose = np.array([[x0, y0] for x0 in nx for y0 in ny])
-
-        self.client_id = cid
+        return source_x, goal_x, obs_x, obs_y
+        
 
     def execute(self):
         rospy.loginfo('executing...')
         res, obstacle_handle = vrep.simxGetObjectHandle(self.client_id,'obstacle',vrep.simx_opmode_oneshot_wait)
+        res, robotino_handle = vrep.simxGetObjectHandle(self.client_id,'robotino',vrep.simx_opmode_oneshot_wait)
+        res, goal_handle = vrep.simxGetObjectHandle(self.client_id,'GOAL',vrep.simx_opmode_oneshot_wait)
 
         # rate = rospy.Rate(20)  # hz
 
@@ -57,13 +83,24 @@ class Conductor:
         launch = roslaunch.scriptapi.ROSLaunch()
         launch.start()
 
-        for obs_pose in np.vstack((self.obs_poses, self.novel_obs_pose)):
-        #for obs_pose in self.novel_obs_pose:
-            vrep.simxStartSimulation(self.client_id,vrep.simx_opmode_blocking)
+        for test_id in range(self.num_max_tests):
+        #for test_id in range(3):
+            source_x, goal_x, obs_x, obs_y = self.get_test_config()
+            rospy.set_param('/start/position/x', float(source_x))
+            rospy.set_param('/goal/position/x', float(goal_x))
+            rospy.set_param('/obstacle/position/x', float(obs_x))
+            rospy.set_param('/obstacle/position/y', float(obs_y))
+        
             _, old_obs_pose = vrep.simxGetObjectPosition(self.client_id, obstacle_handle, -1, vrep.simx_opmode_oneshot)
-            returnCode = vrep.simxSetObjectPosition(self.client_id, obstacle_handle, -1, (obs_pose[0], obs_pose[1], old_obs_pose[2]), vrep.simx_opmode_oneshot)
+            returnCode = vrep.simxSetObjectPosition(self.client_id, obstacle_handle, -1, (obs_x, obs_y, old_obs_pose[2]), vrep.simx_opmode_oneshot)
+            _, old_robotino_pose = vrep.simxGetObjectPosition(self.client_id, robotino_handle, -1, vrep.simx_opmode_oneshot)
+            returnCode = vrep.simxSetObjectPosition(self.client_id, robotino_handle, -1, (source_x, 0, old_robotino_pose[2]), vrep.simx_opmode_oneshot)
+            _, old_goal_pose = vrep.simxGetObjectPosition(self.client_id, goal_handle, -1, vrep.simx_opmode_oneshot)
+            returnCode = vrep.simxSetObjectPosition(self.client_id, goal_handle, -1, (goal_x, 13, old_goal_pose[2]), vrep.simx_opmode_oneshot)
             
-            rospy.loginfo('starting')
+            vrep.simxStartSimulation(self.client_id,vrep.simx_opmode_blocking)
+            rospy.loginfo('test ' + str(test_id) + ' starting')
+            rospy.loginfo('s: ' + str(source_x) + ' g: ' + str(goal_x) + ' obs: ' + str(obs_x) + '-' + str(obs_y))
             launch.launch(recorder_node)
             rospy.loginfo('recorder started')
             time.sleep(0.5)
@@ -73,7 +110,7 @@ class Conductor:
             while process.is_alive():
                 continue
 
-            vrep.simxStopSimulation(self.client_id,vrep.simx_opmode_blocking)
+            vrep.simxStopSimulation(self.client_id, vrep.simx_opmode_blocking)
             time.sleep(0.5)
 
 
@@ -83,11 +120,13 @@ if __name__ == '__main__':
 
     if clientID != -1: # if we connected successfully
         print ('Connected to remote API server')
-    print('starting')
-
+    print('starting tests')
+    
+    # ***
+    
     rospy.init_node('test_conductor', anonymous=True)
 
-    c = Conductor('/home/yigit/Documents/projects/irl_sfm/catkin_ws/src/sfm_planner/data/', clientID)
+    c = Conductor(clientID)
     c.execute()
 
     vrep.simxFinish(-1)
